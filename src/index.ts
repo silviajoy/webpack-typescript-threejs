85096c000e44b2c9919c597fb148ef34617ca49d
silviajoy <floriosg@gmail.com>
2019-03-11 14:58:36 +0100

    threejs setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
-import * as _ from 'lodash';
+import * as THREE from 'three';
 
-function component() {
-    let element = document.createElement('div');
+const scene = new THREE.Scene();
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const camera =new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer =new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry =new THREE.CubeGeometry(1,1,1);
+const material =new THREE.MeshBasicMaterial( {color: 0x00ff00});
+const cube =new THREE.Mesh( geometry, material );
+
+scene.add( cube );
+camera.position.z =5;
+
+console.log(camera)
+
+document.body.appendChild( renderer.domElement );
+
+function render(){    
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+
+render();
+
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     extensions: [ '.ts', '.tsx', '.js' ]
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
